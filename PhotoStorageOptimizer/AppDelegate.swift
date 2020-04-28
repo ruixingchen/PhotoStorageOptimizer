d0d942ecae2b78f22a4617bba79d6d309f7f1283
ruixingchen <dev@ruixingchen.com>
2020-04-28 19:14:23 +0800

    init project

---
+++ PhotoStorageOptimizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoStorageOptimizer
+//
+//  Created by ruixingchen on 2020/4/28.
+//  Copyright © 2020 ruixingchen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'PhotoStorageOptimizer' do
+  use_frameworks!
+  platform :macos, '10.15'
+
+  pod "FileKit", "~> 5.2"
+
+end
